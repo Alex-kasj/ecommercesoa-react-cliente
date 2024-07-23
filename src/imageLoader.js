a280d99577be1e5350317aa04e50bf27383a9a34
Alex-kasj <Kasjken@gmail.com>
2024-07-22 20:12:09 -0500

    Updates

--- src/imageLoader.js
+++
@@ -1,9 +0,0 @@
-function importAll(r) {
-    let images = {};
-    r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
-    return images;
-  }
-  
-  const images = importAll(require.context('./img/gshock', false, /\.(png|jpe?g|svg)$/));
-  
-  export default images;
